e35d56db54f433807a6fe5c4e418be1fd16e8e5a
Tim <53189132+tcausgrove@users.noreply.github.com>
2025-08-12 15:21:51 -0500

    Add files via upload

--- CampingSweet/Views/DrivingDistanceView.swift
+++ CampingSweet/Views/DrivingDistanceView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct DrivingDistanceView: View {
+    @EnvironmentObject var viewModel: ViewModel
+//    @Binding var distance: Measurement<UnitLength>
+//    @State private var distanceString: String = ""
+    @Binding var distanceString: String
+    
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+            Text("Distance (\( viewModel.getDistanceUnitFromSetting() )): ")
+            TextField("Enter value", text: $distanceString)
+               .keyboardType(.decimalPad)
+               .numbersOnly($distanceString, includeDecimal: true, includeNegative: false, digitAllowedAfterDecimal: 1)
+//               .onSubmit {
+//                   distance = Measurement(value: Double(distanceString) ?? 0.0, unit: viewModel.settings.chosenDistance.unit)
+//                }
+        }
     }
 }
 
+
 #Preview {
-    DrivingDistanceView()
+//    let distance: Measurement<UnitLength> = Measurement(value: 0, unit: .miles)
+    DrivingDistanceView(distanceString: .constant("100.0"))
+//    DrivingDistanceView()
+        .environmentObject(ViewModel())
 }
