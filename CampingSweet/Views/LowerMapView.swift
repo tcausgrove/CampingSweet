f0444a3ba654eb1f2f3e24f906b64fc54bba9648
Tim <53189132+tcausgrove@users.noreply.github.com>
2026-01-04 15:34:23 -0600

    v 0.9.6
    Fixed issue with MapsView not opening to correct origin/span

--- CampingSweet/Views/LowerMapView.swift
+++ CampingSweet/Views/LowerMapView.swift
@@ -12,6 +12,10 @@ import Defaults
 
 struct LowerMapView: View {
     var camperName = ""
+    @State private var position: MapCameraPosition = .camera(
+        .init(centerCoordinate: .init(latitude: 37.7749, longitude: -122.4194), distance: 0) // Default values are in San Francisco
+        )
+    
     @Default(.selectedCamperIDKey) var selectedCamperID
     
     @Query var trips: [LogEntry]
@@ -28,15 +32,18 @@ struct LowerMapView: View {
             Text("There are no log entries with location data for \(camperName) in the given time period.")
                 .padding([.leading, .trailing], 32)
         } else {
-            Map() {
+            Map(position: $position, interactionModes: [.pan, .zoom]) {
                 ForEach(trips) { trip in
                     if trip.hasLocationData {
                         Marker(trip.title, coordinate: trip.location!)
                     }
                 }
             }
+            .onAppear {
+                position = .region(centerCoordinate ?? .init(center: CLLocationCoordinate2D(latitude: 39.5, longitude: -98.583), latitudinalMeters: 10000, longitudinalMeters: 10000))
+            }
                 .mapStyle(.standard(
-                    elevation: .flat,
+                    elevation: .realistic,
                     emphasis: .automatic,
                     pointsOfInterest: .excludingAll)
                 )
@@ -52,13 +59,30 @@ struct LowerMapView: View {
 }
 
 
-func logEntryArrayToRegion(sites: [LogEntry]) -> MapCameraBounds? {
+func logEntryArrayToRegion(sites: [LogEntry]) -> MKCoordinateRegion? {
     var averageLatitude: Double = 0.0
     var averageLongitude: Double = 0.0
+    var minLatitude: CLLocationDistance = 90.0
+    var maxLatitude: CLLocationDistance = -90.0
+    var minLongitude: CLLocationDistance = 180.0
+    var maxLongitude: CLLocationDistance = -180.0
     var count: Int = 0
     
     for site in sites {
         if site.hasLocationData {
+            if site.latitude! < minLatitude {
+                minLatitude = site.latitude!
+            }
+            if site.longitude! < minLongitude {
+                minLongitude = site.longitude!
+            }
+            
+            if site.latitude! > maxLatitude {
+                maxLatitude = site.latitude!
+            }
+            if site.longitude! > maxLongitude {
+                maxLongitude = site.longitude!
+            }
             averageLatitude += site.latitude!
             averageLongitude += site.longitude!
             count += 1
@@ -71,7 +95,8 @@ func logEntryArrayToRegion(sites: [LogEntry]) -> MapCameraBounds? {
         return nil
     }
     
-    let rect = MKMapRect(origin: MKMapPoint(CLLocationCoordinate2D(latitude: averageLatitude, longitude: averageLongitude)), size: MKMapSize(width: 100, height: 100))
-    let bounds = MapCameraBounds(centerCoordinateBounds: rect)
-    return (bounds)
+    let mapWidth = (maxLatitude - minLatitude) * 111320  // Approximate number of meters per degree
+    let mapHeight = (maxLongitude - minLongitude) * 111320
+    let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: averageLatitude, longitude: averageLongitude), latitudinalMeters: mapWidth, longitudinalMeters: mapHeight)
+    return (region)
 }
